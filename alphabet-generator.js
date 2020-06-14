7668c615c590fbeb8140efa467588dd27a225807
SevenSecrets <freddieshughes@googlemail.com>
2020-06-14 12:00:04 +0100

    Testing out various markov packages - most are not being maintained etc

--- alphabet-generator.js
+++ alphabet-generator.js
@@ -1,5 +1,19 @@
-var Chain = require('markov-chains').default;
-var corpus = require('')
-var chain = new Chain();
+var fs = require('fs');
+var text = JSON.parse(fs.readFileSync('generator-corpus.json'));
+var Markov = require('markov-strings').default;
+var markov = new Markov(text.list)
 
-module.exports = generator;
+var options = {
+  maxTries: 50,
+  prng: Math.random,
+  filter: (res) => {
+    return res.string.split(' ').length <= 5
+  }
+}
+
+markov.buildCorpus();
+var result = markov.generate(options);
+
+console.log(result);
+
+module.exports;
